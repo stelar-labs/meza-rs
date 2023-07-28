cb18e437e4f2e60eb63f03723dc8c10f27a9c73a
Roy Okello <royokello@protonmail.com>
2023-07-28 15:19:42 +0300

    12023-07-28-1
    resolves #1 , resolves #3 , resolves #4 , resolves #5

--- README.md
+++ README.md
@@ -2,6 +2,64 @@
 
 Meza is an in-memory data table written in Rust.
 
-# Author
+## Author
 
-# Disclaimer
+- Roy R. O. Okello: [Email](mailto:royokello@protonmail.com) & [GitHub](https://github.com/royokello)
+
+## Features
+
+- Data Tables
+- Sort, Filter, Slice & Deduplicate Tables
+- Import & Export for CSV
+- Column Average & Variance
+
+## Usage
+
+### new
+`new:Vec<&str> -> Meza`
+
+```
+let table = meza::Meza::new(vec!["team","goal"]);
+```
+### from_csv
+`from_csv: &str -> Result<Meza<'a>, Box<dyn Error>>`
+### to_csv
+`to_csv:&self, &str -> Result<(), Box<dyn Error>>`
+### sort
+`sort: &mut self, column(&str), order(bool) -> Result<(), Box<dyn Error>>`
+### filter
+`filter: &self, criteria(Fn(&Vec<Cell>) -> bool)) -> Meza`
+### slice
+`slice: &self, start(usize), end(usize) -> Meza`
+### dedup
+`dedup: &mut self`
+### average
+`average: &mut self, column(&str) -> Result<f32, Box<dyn Error>>`
+### variance
+`variance: &mut self, column(&str) -> Result<f32, Box<dyn Error>>`
+
+## License
+
+MIT License
+
+Copyright Stelar Labs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+## Disclaimer
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- src/lib.rs
+++ src/lib.rs
@@ -5,368 +5,350 @@ use std::io::BufReader;
 use std::io::BufRead;
 use std::error::Error;
 use std::path::Path;
-use std::str::FromStr;
 use std::io::Write;
 
 #[derive(Clone, Debug)]
-pub enum MezaType {
-    String,
-    Integer,
-    Floating
+pub struct Cell {
+    pub float32: Option<f32>,
+    pub string: String
 }
+
 #[derive(Clone, Debug)]
 pub struct Meza<'a> {
+    pub averages: Vec<Option<f32>>,
     pub columns: Vec<String>,
-    pub rows: Vec<&'a Vec<String>>,
-    pub types: Vec<MezaType>,
-    pub data: Vec<Vec<String>>
+    pub rows: Vec<&'a Vec<Cell>>,
+    pub column_types: Vec<ColumnType>,
+    pub data: Vec<Vec<Cell>>
 }
 
-impl Meza<'_> {
+#[derive(Clone, Debug)]
+pub enum ColumnType {
+    String,
+    Float32
+}
 
-    pub fn from_csv(csv_path: &str) -> Result<Meza, Box<dyn Error>> {
+impl<'a> Meza<'a> {
+
+    pub fn from_csv(csv_path: &str) -> Result<Meza<'a>, Box<dyn Error>> {
         
         let csv_open = File::open(csv_path)?;
-
+        
         let csv_buffer = BufReader::new(csv_open);
 
-        let mut data: Vec<Vec<String>> = vec![];
-        
         let mut csv_lines = csv_buffer.lines();
+        
+        let column_line = csv_lines.next().ok_or("Empty CSV file")??;
+        
+        let columns: Vec<String> = column_line.split(',').map(|y| y.trim().to_string()).collect();
+        
+        let column_types: Vec<ColumnType> = vec![ColumnType::String; columns.len()];
 
-        let column_line = csv_lines.next().unwrap();
+        let averages = vec![None; columns.len()];
+        
+        let mut data: Vec<Vec<Cell>> = Vec::new();
 
-        let column_line = column_line?;
+        for line_result in csv_lines {
+            let line = line_result?;
+            let row: Vec<String> = line.split(',').map(|y| y.trim().to_string()).collect();
+            let cells = row
+                .iter()
+                .map(|x| Cell {
+                    float32: None,
+                    string: x.to_string(),
+                })
+                .collect();
 
-        let columns: Vec<String> = column_line.split(',').map(|y| y.into()).collect();
-
-        let types: Vec<MezaType> = vec![MezaType::String; columns.len()];
-
-        loop {
-
-            match csv_lines.next() {
-
-                Some(line) => {
-
-                    let line = line?;
-
-                    let row: Vec<String> = line.split(',').map(|y| y.into()).collect();
-
-                    data.push(row)
-                    
-                },
-
-                None => break
-
-            }
-            
+            data.push(cells);
         }
 
-        let result  = Meza { columns, rows: vec![], types, data };
-
-        Ok(result)
+        Ok(Meza {
+            columns,
+            rows: Vec::new(),
+            column_types,
+            data,
+            averages,
+        })
 
     }
 
     pub fn to_csv(&self, csv_path: &str) -> Result<(), Box<dyn Error>> {
-
+        
         if Path::new(csv_path).is_file() {
-            
             fs::remove_file(csv_path)?;
+        }
 
-        };
+        let mut csv_file = OpenOptions::new().write(true).create(true).open(csv_path)?;
 
-        let mut csv_file = OpenOptions::new().append(true).create(true).open(csv_path)?;
+        let column_str = self.columns.join(",");
+        writeln!(csv_file, "{}", column_str)?;
 
-        let mut column_str = self.columns[1..]
-            .iter()
-            .fold(self.columns[0].clone(), |acc, x| format!("{},{}", acc, x));
-
-        column_str.push_str("\n");
-
-        write!(csv_file, "{}", &column_str)?;
-
-        if self.rows.is_empty() {
-
+        if !self.data.is_empty() {
             for row in &self.data {
-
-                let mut row_str = row[1..]
-                    .iter()
-                    .fold(row[0].clone(), |acc, x| format!("{},{}", acc, x));
-
-                row_str.push_str("\n");
-
-                write!(csv_file, "{}", &row_str)?;
-
+                let row_str = row.iter().map(|cell| &cell.string[..]).collect::<Vec<&str>>().join(",");
+                writeln!(csv_file, "{}", row_str)?;
             }
-
         } else {
-
             for row in &self.rows {
-
-                let mut row_str = row[1..]
-                    .iter()
-                    .fold(row[0].clone(), |acc, x| format!("{},{}", acc, x));
-
-                row_str.push_str("\n");
-
-                write!(csv_file, "{}", &row_str)?;
-
+                let row_str = row.iter().map(|cell| &cell.string[..]).collect::<Vec<&str>>().join(",");
+                writeln!(csv_file, "{}", row_str)?;
             }
+        }
 
+        Ok(())
+    }
+
+    pub fn update_csv(&self, csv_path: &str) -> Result<(), Box<dyn Error>> {
+        
+        if self.data.is_empty() && self.rows.is_empty() {
+            return Ok(());
+        }
+
+        let mut csv_file = OpenOptions::new().write(true).create(true).truncate(true).open(csv_path)?;
+
+        if !self.data.is_empty() {
+            for row in self.data.iter() {
+                let row_str = row.iter().map(|cell| &cell.string[..]).collect::<Vec<&str>>().join(",");
+                writeln!(csv_file, "{}", row_str)?;
+            }
+        } else {
+            for row in self.rows.iter() {
+                let row_str = row.iter().map(|cell| &cell.string[..]).collect::<Vec<&str>>().join(",");
+                writeln!(csv_file, "{}", row_str)?;
+            }
         }
 
         Ok(())
 
     }
-    
-    pub fn update_csv(&self, csv_path: &str) -> Result<(), Box<dyn Error>> {
-
-        if Path::new(csv_path).is_file() {
-
-            let mut csv_file = OpenOptions::new().append(true).create(true).open(csv_path)?;
-
-            if self.rows.is_empty() {
-
-                for row in &self.data {
-    
-                    let mut row_str = row[1..]
-                        .iter()
-                        .fold(row[0].clone(), |acc, x| format!("{},{}", acc, x));
-    
-                    row_str.push_str("\n");
-    
-                    write!(csv_file, "{}", &row_str)?;
-    
-                }
-    
-            } else {
-    
-                for row in &self.rows {
-    
-                    let mut row_str = row[1..]
-                        .iter()
-                        .fold(row[0].clone(), |acc, x| format!("{},{}", acc, x));
-    
-                    row_str.push_str("\n");
-    
-                    write!(csv_file, "{}", &row_str)?;
-    
-                }
-    
-            }
-    
-            Ok(())
-
-        } else {
-
-            Err("No file!")?
-
-        }
-
-    }
 
     pub fn sort(&mut self, column: &str, order: bool) -> Result<(), Box<dyn Error>> {
-
-        match self.columns.iter().position(|x| x == column) {
-            
-            Some(i) => {
-
-                if self.rows.is_empty() {
-
-                    match self.types[i] {
-                        MezaType::String => self.data.sort_by(|a, b| a[i].cmp(&b[i])),
-                        MezaType::Integer => self.data.sort_by(|a, b| i128::from_str_radix(&a[i], 10).unwrap().cmp(&i128::from_str_radix(&b[i], 10).unwrap())),
-                        MezaType::Floating => self.data.sort_by(|a, b| f64::from_str(&a[i]).unwrap().total_cmp(&f64::from_str(&b[i]).unwrap()))
-                    }
-
-                    if !order {
-                        self.data.reverse();
-                    }
-
-                } else {
-
-                    match self.types[i] {
-                        MezaType::String => self.rows.sort_by(|a, b| a[i].cmp(&b[i])),
-                        MezaType::Integer => self.rows.sort_by(|a, b| i128::from_str_radix(&a[i], 10).unwrap().cmp(&i128::from_str_radix(&b[i], 10).unwrap())),
-                        MezaType::Floating => self.rows.sort_by(|a, b| f64::from_str(&a[i]).unwrap().total_cmp(&f64::from_str(&b[i]).unwrap()))
-                    }
-
-                    if !order {
-                        self.rows.reverse();
-                    }
-
-                }
-
-                Ok(())
-
-            },
-
-            None => Err("No column!")?
         
-        }
-
-    }
-
-    pub fn filter<F: Fn(&Vec<String>) -> bool>(&self, criteria: F) -> Meza {
-        
-        let mut result = Meza {
-            columns: self.columns.clone(),
-            rows: vec![],
-            types: self.types.clone(),
-            data: vec![]
-        };
+        let i = self
+            .columns
+            .iter()
+            .position(|x| x == column)
+            .ok_or_else(|| Box::<dyn Error>::from("No column!"))?;
 
         if self.rows.is_empty() {
-
-            for row in &self.data {
-
-                if criteria(row) {
-
-                    result.rows.push(row)
-
-                }
-
+            match &self.column_types[i] {
+                ColumnType::String => self.data.sort_by(|a, b| a[i].string.cmp(&b[i].string)),
+                ColumnType::Float32 => self.data.sort_by(|a, b| a[i].float32.partial_cmp(&b[i].float32).unwrap()),
             }
-
         } else {
-                
-            for row in &self.rows {
-
-                if criteria(row) {
-
-                    result.rows.push(row)
-
-                }
-
+            match &self.column_types[i] {
+                ColumnType::String => self.rows.sort_by(|a, b| a[i].string.cmp(&b[i].string)),
+                ColumnType::Float32 => self.rows.sort_by(|a, b| a[i].float32.partial_cmp(&b[i].float32).unwrap()),
             }
-
         }
 
-        result
-
-    }
-
-    pub fn is_empty(&self) -> bool {
-
-        self.rows.is_empty() && self.data.is_empty()
-
-    }
-
-    pub fn new<'a>(columns: &'a [&'a str]) -> Meza<'a> {
-
-        Meza {
-            columns: columns.iter().map(|x| x.to_string()).collect(),
-            rows: vec![],
-            types: vec![MezaType::String; columns.len()],
-            data: vec![]
+        if !order {
+            if self.rows.is_empty() {
+                self.data.reverse();
+            } else {
+                self.rows.reverse();
+            }
         }
 
+        Ok(())
     }
 
-    pub fn column(&self, name: &str) -> Result<Vec<&String>, Box<dyn Error>> {
-
-        match self.columns.iter().position(|x| x == name) {
-
-            Some(i) => {
-
-                let column = if self.rows.is_empty() {
-
-                    self.data.iter().map(|x| &x[i]).collect()
-
-                } else {
-
-                    self.rows.iter().map(|x| &x[i]).collect()
-
-                };
-
-                Ok(column)
-
-            },
-
-            None => Err("No column!")?
-
-        }
-
-    }
-
-    pub fn head(&self, count: usize) -> Meza {
-
+    pub fn filter<F: Fn(&Vec<Cell>) -> bool>(&self, criteria: F) -> Meza {
+        
         let mut meza = Meza {
             columns: self.columns.clone(),
-            data: vec![],
             rows: vec![],
-            types: self.types.clone()
+            column_types: self.column_types.clone(),
+            data: vec![],
+            averages: vec![None; self.columns.len()],
         };
 
         if self.rows.is_empty() {
-        
-            if self.rows.len() >= count {
-
-                meza.rows = self.data[..count].iter().map(|x| x).collect()
-
-            } else {
-
-                meza.rows = self.data.iter().map(|x| x).collect()
-
-            }
-
+            meza.data = self.data.iter().cloned().filter(|row| criteria(row)).collect();
         } else {
-
-            if self.rows.len() >= count {
-
-                meza.rows = self.rows[..count].iter().map(|&x| x).collect()
-
-            } else {
-
-                meza.rows = self.rows.iter().map(|&x| x).collect()
-
-            }
-
+            meza.rows = self.rows.iter().cloned().filter(|row| criteria(row)).collect();
         }
 
         meza
 
     }
 
-    pub fn remove_duplicates(&mut self) {
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty() && self.data.is_empty()
+    }
 
-        if self.rows.is_empty() {
-
-            self.data.sort();
-
-            self.data.dedup();
-
-        } else {
-
-            self.rows.sort();
-
-            self.rows.dedup();
-
+    pub fn new(columns: Vec<&str>) -> Meza {
+        Meza {
+            columns: columns.iter().map(|x| x.to_string()).collect(),
+            rows: vec![],
+            column_types: vec![ColumnType::String; columns.len()],
+            data: vec![],
+            averages: vec![None; columns.len()],
         }
+    }
+
+    pub fn column(&self, name: &str) -> Result<Vec<&Cell>, Box<dyn Error>> {
+        
+        let index = self
+            .columns
+            .iter()
+            .position(|x| x == name)
+            .ok_or_else(|| Box::<dyn Error>::from("No column!"))?;
+
+        let column = if self.rows.is_empty() {
+            self.data.iter().map(|row| &row[index]).collect()
+        } else {
+            self.rows.iter().map(|row| &row[index]).collect()
+        };
+
+        Ok(column)
 
     }
 
-    pub fn append(&mut self, row: Vec<String>) -> Result<(), Box<dyn Error>> {
+    pub fn dedup(&mut self) {
 
-        if self.is_empty() {
-
-            self.data.push(row);
-
-            Ok(())
-
-        } else if !self.data.is_empty() {
-
-            self.data.push(row);
-
-            Ok(())
-
+        if self.rows.is_empty() {
+            self.data.sort();
+            self.data.dedup();
         } else {
-            
-            Err("No Data!")?
-
+            self.rows.sort();
+            self.rows.dedup();
         }
 
     }
     
+    pub fn slice(&self, start: usize, end: usize) -> Meza {
+        let mut meza = Meza {
+            columns: self.columns.clone(),
+            data: vec![],
+            rows: vec![],
+            column_types: self.column_types.clone(),
+            averages: vec![None; self.columns.len()],
+        };
+
+        if self.rows.is_empty() {
+            meza.data = if end <= self.data.len() {
+                self.data[start..end].to_vec()
+            } else {
+                self.data.clone()
+            };
+        } else {
+            meza.rows = if end <= self.rows.len() {
+                self.rows[start..end].to_vec()
+            } else {
+                self.rows.clone()
+            };
+        }
+
+        meza
+    }
+
+    pub fn average(&mut self, column: &str) -> Result<f32, Box<dyn Error>> {
+        
+        let i = match self.columns.iter().position(|x| x == column) {
+            Some(i) => i,
+            None => Err("Column not found!")?,
+        };
+        
+        let mut sum = 0.0;
+
+        let mut count = 0;
+
+        if self.data.is_empty() {
+            for row in &self.rows {
+                if let Some(cell) = row.get(i) {
+                    match cell.float32 {
+                        Some(value) => {
+                            sum += f32::from(value);
+                            count += 1;
+                        }
+                        None => continue,
+                    }
+                }
+            }
+        } else {
+            for row in &self.data {
+                if let Some(cell) = row.get(i) {
+                    match cell.float32 {
+                        Some(value) => {
+                            sum += f32::from(value);
+                            count += 1;
+                        }
+                        None => continue,
+                    }
+                }
+            }
+        }
+        
+        let avg = if count > 0 {
+            sum / count as f32
+        } else {
+            0.0
+        };
+
+        self.averages[i] = Some(avg);
+
+        Ok(avg)
+
+    }
+    
+    pub fn variance(&mut self, column: &str) -> Result<f32, Box<dyn Error>> {
+        
+        let i = self
+            .columns
+            .iter()
+            .position(|x| x == column)
+            .expect("Column not found!");
+
+        let average = match self.averages[i] {
+            Some(avg) => avg,
+            None => self.average(column)?
+        };
+
+        let values: Vec<f32> = if self.rows.is_empty() {
+            self.data
+                .iter()
+                .filter_map(|row| row[i].float32)
+                .collect()
+        } else {
+            self.rows
+                .iter()
+                .filter_map(|row| row[i].float32)
+                .collect()
+        };
+
+        let squared_differences: Vec<f32> = values
+            .iter()
+            .map(|value| (value - average).powi(2))
+            .collect();
+
+        let sum_squared_differences: f32 = squared_differences.iter().sum();
+
+        let variance = sum_squared_differences / (values.len() as f32);
+
+        Ok(variance)
+
+    }
+
+}
+
+use std::cmp::Ordering;
+
+impl Ord for Cell {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.string.cmp(&other.string)
+    }
+}
+
+impl PartialOrd for Cell {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Eq for Cell {}
+
+impl PartialEq for Cell {
+    fn eq(&self, other: &Self) -> bool {
+        self.string == other.string
+    }
 }
